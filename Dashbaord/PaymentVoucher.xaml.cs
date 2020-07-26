b89e677318141391b0ba2e49aadb4819c2e95f30
vermavashish <vermavashish@gmail.com>
2020-07-26 12:00:58 +0530

    Payment

--- Dashbaord/PaymentVoucher.xaml.cs
+++ Dashbaord/PaymentVoucher.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using GravitonLibrary;
+using GravitonLibrary.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,9 +22,147 @@ namespace Dashbaord
     /// </summary>
     public partial class PaymentVoucher : UserControl
     {
+        private List<LedgerModel> availableLedgers = new List<LedgerModel>();
+        int AccountId = 0;
+        int ParticularId = 0;
         public PaymentVoucher()
         {
             InitializeComponent();
+            LoadListData();
+            WireUpLists();
+        }
+
+        class PaymentBill
+        {
+            public string reference { get; set; }
+            public string emi { get; set; }
+            public string due_date { get; set; }
+            public string amount { get; set; }
+        }
+
+        private void LoadListData()
+        {
+            availableLedgers = GlobalConfig.Connection.GetLedger_All();
+        }
+
+        private void WireUpLists()
+        {
+            AccountLedgerCombobox.ItemsSource = null;
+
+            AccountLedgerCombobox.ItemsSource = availableLedgers;
+            AccountLedgerCombobox.DisplayMemberPath = "ledger_name";
+
+            ParticularLedgerCombobox.ItemsSource = null;
+
+            ParticularLedgerCombobox.ItemsSource = availableLedgers;
+            ParticularLedgerCombobox.DisplayMemberPath = "ledger_name";
+        }
+
+        private void AccountLedgerCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LedgerModel model = (LedgerModel)AccountLedgerCombobox.SelectedItem;
+            AccountCurrentBalanceDataValue.Text = model.ledger_opening_balance.ToString();
+            AccountId = model.lid;
+
+        }
+
+        private void ParticularLedgerCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LedgerModel model = (LedgerModel)ParticularLedgerCombobox.SelectedItem;
+            ParticularCurrentBalanceDataValue.Text = model.ledger_opening_balance.ToString();
+            ParticularId = model.lid;
+        }
+
+        private void StartDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ValidateForm())
+            {
+                BillwiseDataGrid.Items.Clear();
+
+                int amount = int.Parse(AmountTextbox.Text);
+                //DateTime? n = StartDatePicker.SelectedDate;
+                int num = int.Parse(NumberOfEmiTextbox.Text);
+                string date = StartDatePicker.SelectedDate.ToString().Split(' ').First();
+                for (int i = 0; i < num; i++)
+                {
+                    PaymentBill p = new PaymentBill();
+                    p.reference = "New";
+                    p.emi = $"E0{i+1}";
+                    p.due_date = i == 0 ? date : NextMonth(date);
+                    p.amount = $"{(double)amount/num}";
+                    date = p.due_date;
+
+                    BillwiseDataGrid.Items.Add(p);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Fill in the Details Properly!");
+            }
+        }
+
+        private string NextMonth(string date)
+        {
+            string nextDate = date;
+            string[] dateList = nextDate.Split('-');
+            int month = int.Parse(dateList[1]);
+            if(month < 12)
+            {
+                if(month < 9)
+                {
+                    nextDate = $"{dateList[0]}-0{month + 1}-{dateList[2]}";
+                }
+                else
+                {
+                    nextDate = $"{dateList[0]}-{month + 1}-{dateList[2]}";
+                }
+            }else if(month == 12)
+            {
+                int year = int.Parse(dateList[2]);
+                nextDate = $"{dateList[0]}-0{1}-{year + 1}";
+            }
+            return nextDate;
+        }
+
+        private bool ValidateForm()
+        {
+            bool output = true;
+            if(AccountLedgerCombobox.SelectedItem == null)
+            {
+                output = false;
+            }
+            if (ParticularLedgerCombobox.SelectedItem == null)
+            {
+                output = false;
+            }
+            if(AmountTextbox.Text.Length == 0)
+            {
+                output = false;
+            }
+            if (NumberOfEmiTextbox.Text.Length == 0)
+            {
+                output = false;
+            }
+            if(DatePicker.SelectedDate == null)
+            {
+                output = false;
+            }
+            return output;
+        }
+
+        private void StartDatePicker_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                VoucherModel voucherModel = new VoucherModel();
+                ParticularModel particularModel = new ParticularModel();
+
+                voucherModel.v_date = DatePicker.SelectedDate.ToString().Split(' ').First();
+                voucherModel.v_number = 1;
+                voucherModel.vtype = "Payment";
+                voucherModel.account = 
+                //MessageBox.Show("Hello");
+            }
         }
     }
 }
